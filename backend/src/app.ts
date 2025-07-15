46ed8004df95bac9cce6245606c424a25f4e1cd8
Himanshu <himanshunegi378@gmail.com>
2025-07-15 21:21:15 +0530

    cors update

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -18,7 +18,7 @@ const app = express();
 
 // Middlewares
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: ['http://localhost:5173', 'https://expenselm.timercards.com', 'https://www.expenselm.expenselm.timercards.com'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
